cb30364c57503c87e9e306ab2f182e6f500c9fb6
Phuc-code-2001 <63900676+Phuc-code-2001@users.noreply.github.com>
2021-04-17 15:21:45 +0700

    Add files via upload

--- assets/graphic.py
+++ assets/graphic.py
@@ -1,21 +1,21 @@
 from tkinter import *
+from tkinter.filedialog import *
+from tkinter.messagebox import *
 
 class UI(Tk):
     def __init__(self, app):
         super().__init__()
         self.app = app
-        self.WIDTH = 600
-        self.HEIGHT = 400
-        self.X = 150
-        self.Y = 200
+        self.WIDTH = 850
+        self.HEIGHT = 500
+        self.X = 600
+        self.Y = 150
         self.geometry(str(self.WIDTH) + 'x' +  str(self.HEIGHT) + '+' + str(self.X) + '+' + str(self.Y))
         self.title("Youtube Downloader")
-        try:
-            self.icon = PhotoImage(file="icon.png")
-            self.iconphoto(False, self.icon)
-        except: ...
+        self.icon = PhotoImage(file="icon.png")
+        self.iconphoto(False, self.icon)
         self.configure(background='#21AADD')
-        self.resizable(0, 0)
+        self.resizable(1, 1)
 
         self.setLayout()
 
@@ -29,18 +29,18 @@ class UI(Tk):
       
         self.checkBtn = Button(self.TopLayout, text="Check", font="Constantia 12", command=lambda: self.app.check())
         self.dlBtn = Button(self.TopLayout, text="Download", font="Constantia 12", command=lambda: self.app.download_callback())
-        self.reBtn = Button(self.TopLayout, text="Reset", font="Constantia 12", command=lambda: self.app.reset())
-        self.pathBtn = Button(self.TopLayout, text="...", font="Constantia 12", command= lambda: self.app.choosePath())
-        self.clshtr = Button(self.TopLayout, text='Clear', font="Constantia 12", command=lambda: self.app.loger.clear())
+        self.openYTBtn = Button(self.TopLayout, text="YouTube", font="Constantia 12", command=lambda: self.app.openYouTube())
+        self.pathBtn = Button(self.TopLayout, text="...", font="Constantia 12", command= lambda: self.setPath())
+        self.clshtr = Button(self.TopLayout, text='Clear', font="Constantia 12", command=lambda: self.app.clearURL())
         self.openBtn = Button(self.TopLayout, text="Open", font="Constantia 12", command=lambda: self.app.openCurrentFolder())
-        self.pathLabel = Label(self.TopLayout, text=": ./Videos", font="Constantia 12", bg=self['bg'])
+        self.pathLabel = Label(self.TopLayout, text=": ./videos", font="Constantia 12", bg=self['bg'])
 
 
         self.pathBtn.place(relx=0.025, relwidth=0.05, y=50, height=20)
         self.pathLabel.place(relx=0.075, y=50, height=20)
         self.checkBtn.place(relx=0.025, y=80)
         self.dlBtn.place(relx=0.14, y=80)
-        self.reBtn.place(relx=0.3, y=80)
+        self.openYTBtn.place(relx=0.3, y=80)
         self.TopLayout.update()
         self.clshtr.place(relx=0.025, y=130, width=self.checkBtn.winfo_width())
         self.openBtn.place(relx=0.14, y=130)
@@ -74,3 +74,27 @@ class UI(Tk):
 
         self.urlBefore = Listbox(self.BottomLayout, selectmode="SINGLE", height=5, font="Constantia 12")
         self.urlBefore.place(relx=0.05, relwidth=0.9, rely=0.1, relheight=0.8)
+
+    def clean(self):
+        self.board.delete(0, END)
+
+    def print(self, message):
+        for text in message:
+            self.board.insert(END, text)
+
+    def setPath(self):
+        path = askdirectory()
+        if path:
+            self.app.savePath = path
+            self.pathLabel.config(text=path)
+
+    def load(self, urlbox):
+        for url in urlbox:
+            self.insert(url)
+    
+    def insert(self, url):
+        self.urlBefore.insert(END, url)
+
+    def clearURL(self):
+        self.urlBefore.delete(0, END)
+    
